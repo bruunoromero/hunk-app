6b795e3c9dd63a6f3427a6c17e1a9d110e1c3d4b
brunoromero <brunoromero172@hotmail.com>
2018-07-18 22:29:45 -0300

    added react native elements and started step1 for creating a new list

--- src/screens/list-create/step1.js
+++ src/screens/list-create/step1.js
@@ -1,23 +1,59 @@
 import React from "react";
-import {
-  Text,
-  Icon,
-  Item,
-  Input,
-  Header,
-  Button,
-  Container
-} from "native-base";
+import { Platform, View } from "react-native";
+import { Container, Text } from "native-base";
+import { SearchBar } from "react-native-elements";
+import { withHandlers, compose, lifecycle, withState } from "recompose";
 
-export default () => (
-  <Container>
-    <Item>
-      <Icon name="ios-search" />
-      <Input placeholder="Search" />
-      <Icon name="ios-people" />
-    </Item>
-    <Button transparent>
-      <Text>Search</Text>
-    </Button>
-  </Container>
+import Header from "../../components/header/header";
+
+const data = [
+  1337,
+  "janeway",
+  {
+    lots: "of",
+    different: {
+      types: 0,
+      data: false,
+      that: {
+        can: {
+          be: {
+            quite: {
+              complex: {
+                hidden: ["gold!"]
+              }
+            }
+          }
+        }
+      }
+    }
+  },
+  [4, 2, "tree"]
+];
+
+const enhance = compose(
+  withState("searchText", "setSearchText", ""),
+  withHandlers({
+    resetSearchText: ({ setSearchText }) => () => {
+      setSearchText("");
+    }
+  })
 );
+
+const platform = Platform.OS;
+
+export default enhance(({ setSearchText, resetSearchText, searchText }) => (
+  <Container>
+    <Header title="List Create" />
+    <SearchBar
+      lightTheme
+      value={searchText}
+      platform={platform}
+      onCancel={resetSearchText}
+      onChangeText={setSearchText}
+      icon={{ type: "font-awesome", name: "search" }}
+    />
+    <View>
+      <Text>{searchText}</Text>
+    </View>
+  </Container>
+));
